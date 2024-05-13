7b39461d1c48a85495b476f82a8ce263ebf1fd78
damien.morard <damien.morard@unige.ch>
2024-05-13 10:01:46 +0200

    Update the mcc script to be correct by including the iscolored parameter

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,20 +1,15 @@
 import SVSKit
 import Foundation
 
-//func ctlFireability(net: PetriNet, marking: Marking) {
-//  let parserCTL = CTLParser()
-//  let dicCTLFormulas = parserCTL.loadCTL(filePath: "CTLFireability.xml")
-//  for (nameFormula, ctlFormula) in dicCTLFormulas.sorted(by: {$0.key < $1.key}) {
-//    let ctlReduced = CTL(formula: ctlFormula, net: net, canonicityLevel: .full, simplified: false, debug: false).queryReduction()
-//    let b = ctlReduced.eval(marking: marking)
-//    print("FORMULA \(nameFormula) \(b.description.uppercased()) TECHNIQUES IMPLICIT QUERY_REDUCTION")
-//  }
-//}
 
+/// Evaluate a CTL query by calling the library SVSKit
+/// - Parameters:
+///   - net: Current Petri net
+///   - marking: Current marking from a Petri net
+///   - nameFile: The file containing CTL formulas
 func eval(net: PetriNet, marking: Marking, nameFile: String) {
   let parserCTL = CTLParser()
   let dicCTLFormulas = parserCTL.loadCTL(filePath: nameFile)
-//  let dicCTLFormulas = parserCTL.loadCTL(filePath: "ReachabilityFireability.xml")
   for (nameFormula, ctlFormula) in dicCTLFormulas.sorted(by: {$0.key < $1.key}) {
     let ctlReduced = CTL(formula: ctlFormula, net: net, canonicityLevel: .full, simplified: false, debug: false).queryReduction()
     let b = ctlReduced.eval(marking: marking)
@@ -22,46 +17,46 @@ func eval(net: PetriNet, marking: Marking, nameFile: String) {
   }
 }
 
-// For many examples
+// Return the results for the MCC competiton, this script is made to be inserted in the MCC virtual machine
 func main() {
-  let arguments = CommandLine.arguments
-  let category = arguments[1]
-  let parserPN = PnmlParser()
-  let (net, marking) = parserPN.loadPN(filePath: "model.pnml")
-  var newCapacity: [String: Int] = [:]
-  for place in net.places {
-    newCapacity[place] = Int.max-1
+  if let data = FileManager.default.contents(atPath: "iscolored") {
+    var isColoredString: String = ""
+    for b in data {
+      isColoredString += String(UnicodeScalar(UInt8(b)))
+    }
+    let arguments = CommandLine.arguments
+    let category = arguments[1]
+    if isColoredString == "FALSE" {
+      let parserPN = PnmlParser()
+      let (net, marking) = parserPN.loadPN(filePath: "model.pnml")
+      var newCapacity: [String: Int] = [:]
+      for place in net.places {
+        newCapacity[place] = Int.max-1
+      }
+      net.capacity = newCapacity
+      
+      switch category {
+      case "CTLFireability":
+        eval(net: net, marking: marking, nameFile: "CTLFireability.xml")
+      case "ReachabilityFireability":
+        eval(net: net, marking: marking, nameFile: "ReachabilityFireability.xml")
+      default:
+        print("DO_NOT_COMPETE")
+      }
+    } else {
+      switch category {
+      case "CTLFireability":
+        print("CANNOT_COMPUTE")
+      case "ReachabilityFireability":
+        print("CANNOT_COMPUTE")
+      default:
+        print("DO_NOT_COMPETE")
+      }
+      print("CANNOT_COMPUTE")
+    }
+  } else {
+    print("CANNOT_COMPUTE")
   }
-  net.capacity = newCapacity
-  
-  switch category {
-  case "CTLFireability":
-    eval(net: net, marking: marking, nameFile: "CTLFireability.xml")
-  case "ReachabilityFireability":
-    eval(net: net, marking: marking, nameFile: "ReachabilityFireability.xml")
-  default:
-    print("DO_NOT_COMPETE")
-  }
-//  let pnmlPath = "model.pnml"
-//  let ctlPath = "CTLFireability.xml"
-//  let parserPN = PnmlParser()
-//  let (net, marking) = parserPN.loadPN(filePath: pnmlPath)
-//  var newCapacity: [String: Int] = [:]
-//  for place in net.places {
-//    newCapacity[place] = Int.max-1
-//  }
-//  net.capacity = newCapacity
-//  print(net.places.count)
-//  print(net.transitions.count)
-//  let parserCTL = CTLParser()
-//  let dicCTLFormulas = parserCTL.loadCTL(filePath: ctlPath)
-//
-//  for (nameFormula, ctlFormula) in dicCTLFormulas {
-//    let ctlReduced = CTL(formula: ctlFormula, net: net, canonicityLevel: .full, simplified: false, debug: false).queryReduction()
-//    let b = ctlReduced.eval(marking: marking)
-//    print("FORMULA \(nameFormula) \(b) TECHNIQUES ...")
-//  }
-
 }
 
 main()
